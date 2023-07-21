dff8a760777499981331cf98cc34156da64cf8af
emrulla muhter <emrullauigur@gmai.com>
2023-07-21 16:42:02 +0300

    data structure

--- src/main/java/BigOsingleLoop.java
+++ src/main/java/BigOsingleLoop.java
@@ -1,7 +1,8 @@
 public class BigOsingleLoop {
     public static void main(String[] args) {
         int numberOfDataOperation = 0;
-        int n = 100;
+        long n = 100;
+        long m=200;
         long startTime;
         long endTime;
         System.out.println("Input size n is =" + n);
@@ -9,13 +10,11 @@ public class BigOsingleLoop {
         startTime = System.currentTimeMillis();
 
         for (int i = 0; i < n; i++) {
-
-
             numberOfDataOperation += 1;
 
         }
         endTime = System.currentTimeMillis();
-        System.out.println("number of operation " + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
+        System.out.println("number of operation with for loop " + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
 
         //task 2 nested loop
         startTime = System.currentTimeMillis();
@@ -26,7 +25,51 @@ public class BigOsingleLoop {
             }
         }
         endTime = System.currentTimeMillis();
-        System.out.println("number of operation " + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
+        System.out.println("number of operation with nested loop " + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
+
+
+
+
+    //Task 3
+
+        startTime = System.currentTimeMillis();
+            for (int b = 0; b <n ; b++) {
+            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                numberOfDataOperation+=1;
+            }
+        }}
+        endTime = System.currentTimeMillis();
+        System.out.println("number of operation with triple nested loop " + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
+// Task 4  n and m nested loop (n*m)
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <m ; j++) {
+                numberOfDataOperation+=1;
+            }
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("number of operation with  nested loop (n*m)" +  + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
+// task 4.5 n and m consecutive loops (n+m)
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i <n ; i++) {
+
+        }
+        for (int i=0;i<m;i++){
+            numberOfDataOperation+=1;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("number of operation with  nested loop (n+m)" +  + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
+//task 5 logaritmic complexity
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < n; i*=2) {
+            numberOfDataOperation += 1;
+
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("number of operations  " + numberOfDataOperation + " in " + (endTime - startTime) + "mili second");
 
 
     }
